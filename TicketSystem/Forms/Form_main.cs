79d9302d5a60a766a050624b7eddc29911e9d00c
Iago <iago.formes.r@gmail.com>
2023-02-09 17:02:27 -0300

    Updating Git to add Ticket System Lib

---
+++ TicketSystem/Forms/Form_main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketSystem.Forms
+{
+    public partial class Form_main : Form
+    {
+        public Form_main()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void btn_novo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chamado criado com sucesso!");
+        }
+    }
+}
